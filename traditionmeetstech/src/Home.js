a10d917d07e4a34a6c4247c38131c06709f2d92e
lauracolacicco <laura.colacicco@studio.unibo.it>
2024-04-05 13:08:24 +0200

    modifiche home

--- traditionmeetstech/src/Home.js
+++ traditionmeetstech/src/Home.js
@@ -1,16 +1,32 @@
 import './Home.css';
 
 function Home(){
+    function scrollToParagraph() {
+        var paragraph = document.getElementById("paragrafo");
+        paragraph.scrollIntoView({ behavior: 'smooth' });
+      }
+
     return (
-      <header>
+    <div className="body">
+      <div className="header">
         <div class="overlay">
           <h1>Simply The Best</h1>
           <h3>Reasons for Choosing US</h3>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab.</p>
           <br />
-          <button>READ MORE</button>
+          <button  onclick="scrollToParagraph()">READ MORE</button>
         </div>
-      </header>
+      </div>
+
+      <div id="paragrafo" className='paragrafo'>
+        <h3>Questo è il paragrafo di destinazione</h3>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab.</p>
+      </div>
+
+        <div className="divisore">
+            
+        </div>
+    </div>  
     );
   }
   
