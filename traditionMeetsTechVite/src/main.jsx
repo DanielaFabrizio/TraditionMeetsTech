95bf1fcaee9b2a00c5b196608fa81a4ecdd6614b
manuel091002 <manuelflagelli02@gmail.com>
2024-04-05 20:01:45 +0200

    inserito chatbot a livello index (quando mi sposto tra un componente e un altro, il chatbot rimane invariato), commentata riga bootstrap perché dà errore

--- traditionMeetsTechVite/src/main.jsx
+++ traditionMeetsTechVite/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css'; // Importa il file CSS di Bootstrap
+//import 'bootstrap/dist/css/bootstrap.min.css'; // Importa il file CSS di Bootstrap
 import './index.css'
 import Router from './router/Router.jsx'
 
