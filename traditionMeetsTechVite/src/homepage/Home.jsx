b48a7dca18a28cdab5dd1a5f55a235578cf06720
lauracolacicco <laura.colacicco@studio.unibo.it>
2024-04-21 12:34:23 +0200

    aggiunta paragrafo immagini

--- traditionMeetsTechVite/src/homepage/Home.jsx
+++ traditionMeetsTechVite/src/homepage/Home.jsx
@@ -10,7 +10,7 @@ function Home(){
     <div className="body">
       <div className="header">
         <div class="overlay">
-          <h1>Simply The Best</h1>
+          <h1>Tradition Meets Tech</h1>
           <h3>Reasons for Choosing US</h3>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab.</p>
           <br />
@@ -20,46 +20,76 @@ function Home(){
         </div>
       </div>
 
-      <div id="paragrafo" className='paragrafo'>
-        <h3>Questo è il paragrafo di destinazione</h3>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab.</p>
+      <div id="sezione" className='sezione'>
+        <div className='paragrafo'>
+        <h4>La storia del lavoro</h4>
+        </div>
       </div>
 
-        <div className="divisore">
-          <div class="row">
+      <div id="sezione2" className='sezione'>
+          <div className='paragrafo'>
+          <h3>Questo è il paragrafo di destinazione</h3>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab.</p>
+          </div>
+      </div>
 
-             <div class="col-8"> 
-                <div className='overlay-divisore'>
-                  <h1>Simply The Best</h1>
-                  <h3>Reasons for Choosing US</h3>
-                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab.</p>
-                  <br />
-                </div> 
-              </div>
+      <div id="sezione" className='sezione'>
+          <div className='paragrafo'>
+          <h3>Questo è il paragrafo di destinazione</h3>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab.</p>
+          </div>
+      </div>
 
 
-            <div class="col-4">
-              <div class="item-hints">
-                  <div class="hint" data-position="4">
-                    <span class="hint-radius"></span>
-                    <span class="hint-dot">Vai allo studio</span>
-                    <div class="hint-content do--split-children">
-                      <p>Use Navbar to navigate the website quickly and easily.</p>
-                    </div>
-                  </div>
-              </div>
+      <div>
+
+        <div class="demo coursemeal">
+            <div class="coursemeal__div ">
+                
+                <picture className="responsive-img">
+                    <source media="(min-width: 992px)" srcset="https://www.ilponte.com/wp-content/uploads/2022/05/Falegnameria.jpg"/>
+                    <source media="(min-width: 767px)" srcset="https://i.postimg.cc/1RTdCYhB/appetizer-img-2.jpg"/>
+                    <source srcset="https://i.postimg.cc/TwnctQr7/appetizer-img-3.jpg"/>
+                    <img src="https://www.ilponte.com/wp-content/uploads/2022/05/Falegnameria.jpg" alt="Falegname"/>
+                </picture>     
+                
+                <div class="coursemeal-info">
+                    <a href="#" class="coursemeal-info__link">Falegname</a>
+                </div>
             </div>
 
-              
+            <div class="coursemeal__div">
+                
+                <picture class="responsive-img">
+                    <source media="(min-width: 992px)" srcset="https://i.postimg.cc/V6NCCRK0/maindish-img-1.jpg"/>
+                    <source media="(min-width: 767px)" srcset="https://i.postimg.cc/9XYj186s/maindish-img-2.jpg"/>
+                    <source srcset="https://i.postimg.cc/pLY8dt4q/maindish-img-3.jpg"/>
+                    <img src="https://i.postimg.cc/V6NCCRK0/maindish-img-1.jpg" alt="Main Course"/>
+                </picture> 
+
+                <div class="coursemeal-info">
+                    <a href="#" class="coursemeal-info__link">Main Course</a>
+                </div>            
+            </div>
+
+            <div class="coursemeal__div">
+                
+                <picture class="responsive-img">
+                    <source media="(min-width: 992px)" srcset="https://i.postimg.cc/9FfLh6ZZ/dessert-img-1.jpg"/>
+                    <source media="(min-width: 767px)" srcset="https://i.postimg.cc/FsRXBnKn/dessert-img-2.jpg"/>
+                    <source srcset="https://i.postimg.cc/ZKcFCVFZ/dessert-img-3.jpg"/>
+                    <img src="https://i.postimg.cc/ZKcFCVFZ/dessert-img-1.jpg" alt="Desserts"/>
+                </picture>
+
+                <div class="coursemeal-info">
+                    <a href="#" class="coursemeal-info__link">Dessert</a>
+                </div>              
+            </div>
+
+
           </div>
-         
-        </div>
-
-        <div id="paragrafo2" className='paragrafo'>
-        <h3>Questo è il paragrafo di destinazione</h3>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab.</p>
-       </div>
-
+      </div>
+    
        <div className="footer"> 
 
         </div>
