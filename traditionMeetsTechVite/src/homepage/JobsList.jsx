467fcc5ef64fc14d817fa452e4bd1ec47ceb8a85
DanielaFabrizio <145684148+DanielaFabrizio@users.noreply.github.com>
2024-04-25 10:21:06 +0200

    lista lavori

---
+++ traditionMeetsTechVite/src/homepage/JobsList.jsx
@@ -0,0 +1,82 @@
+import './JobsList.css';
+
+function JobsList(){
+    function scrollToParagraph() {
+        var paragraph = document.getElementById("paragrafo");
+        paragraph.scrollIntoView({ behavior: 'smooth' });
+      }
+
+    return (
+    <div className="body">
+        <div className="header">
+            <div class="overlay">
+            <h1>Simply The Best</h1>
+            <h3>Reasons for Choosing US</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab.</p>
+            <br />
+            <button  onClick={scrollToParagraph}> 
+                    <span class="box"> Hover! </span>
+            </button>
+            </div>
+        </div>
+
+        <div id="paragrafo" className='paragrafo'>
+            <h3>Questo è il paragrafo di destinazione</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab.</p>
+        </div>
+
+        <div className="divisore">
+            <div className="colonna">
+                <div className="row">
+                    <div class="cardJob">
+                        <p class="c-txt">Falegname</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div class="cardJob">
+                        <p class="c-txt">Calzolaio</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="colonna">
+                <div className="row">
+                    <div class="cardJob">
+                        <p class="c-txt">Pellettiere</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div class="cardJob">
+                        <p class="c-txt">Agricoltore</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="colonna">
+                <div className="row">
+                    <div class="cardJob">
+                        <p class="c-txt">Panettiere</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div class="cardJob">
+                        <p class="c-txt">Hover Me</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="paragrafo2" className='paragrafo'>
+            <h3>Questo è il paragrafo di destinazione</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab.</p>
+        </div>
+
+        <div className="footer"> 
+
+        </div>
+
+    </div>  
+    );
+  }
+  
+  export default JobsList;
