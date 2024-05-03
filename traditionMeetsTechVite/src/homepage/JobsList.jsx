b50842d757f33353650e6cbbe016dc8b98d4a7eb
DanielaFabrizio <145684148+DanielaFabrizio@users.noreply.github.com>
2024-05-03 10:46:21 +0200

    non mi ricordo che ho fatto hihi

--- traditionMeetsTechVite/src/homepage/JobsList.jsx
+++ traditionMeetsTechVite/src/homepage/JobsList.jsx
@@ -1,4 +1,7 @@
 import './JobsList.css';
+import Footer from './Footer.jsx';
+import Header from './Header.jsx';
+import Paragrafo from './Paragrafo.jsx';
 
 function JobsList(){
     function scrollToParagraph() {
@@ -8,17 +11,9 @@ function JobsList(){
 
     return (
     <div className="body">
-        <div className="header">
-            <div class="overlay">
-            <h1>Simply The Best</h1>
-            <h3>Reasons for Choosing US</h3>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab.</p>
-            <br />
-            <button  onClick={scrollToParagraph}> 
-                    <span class="box"> Hover! </span>
-            </button>
-            </div>
-        </div>
+        <Header onScroll={scrollToParagraph}
+        urlBackground='https://www.unifor.br/documents/20143/573160/foto-materia-unifor-noticias-futuro-logo-ali-getty-images-800.jpg/6b13a8f7-bed7-5c38-effb-8d3f02982e45?t=1620930442724'/>
+
 
         <div id="paragrafo" className='paragrafo'>
             <h3>Questo è il paragrafo di destinazione</h3>
@@ -28,12 +23,12 @@ function JobsList(){
         <div className="divisore">
             <div className="colonna">
                 <div className="row">
-                    <div class="cardJob">
+                    <div class="cardJob" id="cardJob1">
                         <p class="c-txt">Falegname</p>
                     </div>
                 </div>
                 <div className="row">
-                    <div class="cardJob">
+                    <div class="cardJob" id="cardJob2">
                         <p class="c-txt">Calzolaio</p>
                     </div>
                 </div>
@@ -41,12 +36,12 @@ function JobsList(){
 
             <div className="colonna">
                 <div className="row">
-                    <div class="cardJob">
+                    <div class="cardJob" id="cardJob3">
                         <p class="c-txt">Pellettiere</p>
                     </div>
                 </div>
                 <div className="row">
-                    <div class="cardJob">
+                    <div class="cardJob" id="cardJob4">
                         <p class="c-txt">Agricoltore</p>
                     </div>
                 </div>
@@ -54,7 +49,7 @@ function JobsList(){
 
             <div className="colonna">
                 <div className="row">
-                    <div class="cardJob">
+                    <div class="cardJob" id="cardJob5">
                         <p class="c-txt">Panettiere</p>
                     </div>
                 </div>
@@ -71,20 +66,7 @@ function JobsList(){
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab.</p>
         </div>
 
-        <div className="footer"> 
-              <div class="card text-center">
-                <div class="card-header">
-                  Featured
-                </div>
-                <div class="card-body">
-                  <h5 class="card-title">Componenti del gruppo</h5>
-                  <p class="card-text">Colacicco Laura | Daniela Fabrizio | Manuel Flagelli </p>
-                  <button  onClick={scrollToParagraph}> 
-                    <span class="box"> Curiosità sul progetto!! </span>
-                  </button>
-                </div>
-              </div>
-        </div>
+        <Footer onScroll={scrollToParagraph}/>
 
     </div>  
     );
