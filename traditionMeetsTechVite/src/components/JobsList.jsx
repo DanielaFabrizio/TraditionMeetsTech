c1a000bb353f5310c752ab073d5b1b6b78ed2e1b
lauracolacicco <laura.colacicco@studio.unibo.it>
2024-05-25 15:47:37 +0200

    modifiche joblist

--- traditionMeetsTechVite/src/components/JobsList.jsx
+++ traditionMeetsTechVite/src/components/JobsList.jsx
@@ -108,16 +108,7 @@ function JobsList(){
             </div>
         </div>
 
-        <div id='button-studio'>
-            <Link to="/jobs-list" className="cta">
-            
-                    <span class="hover-underline-animation"> Vai allo studio </span>
-                    <svg id="arrow-horizontal" xmlns="http://www.w3.org/2000/svg" width="30" height="10" viewBox="0 0 46 16">
-                    <path id="Path_10" data-name="Path 10" d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z" transform="translate(30)"></path>
-                    </svg>
-            
-            </Link>
-        </div>
+        
 
         <Footer/>
 
